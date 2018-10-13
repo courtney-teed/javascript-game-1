aec8a384cf1a105ee24fa93939acb84006401071
Courtney Teed <courtneyteed@Courtneys-MBP.attlocal.net>
2018-10-13 13:11:17 -0700

    Rearrange JS to fix functionality

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
-
 //define variables
 var button = document.getElementsByTagName('button')[0];
-//Event handler
-button.onclick = shakeBall;
 
 var yourFortune = document.querySelector('#fortune-wrapper p span');
 
 //create placeholder text and append to yourFortune paragraph
 var fortuneTextPlaceholder = document.createTextNode('');
 yourFortune.appendChild(fortuneTextPlaceholder);
+
+
+var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
 //function
 var shakeBall = function() {
@@ -24,3 +23,6 @@ var shakeBall = function() {
 	//add class to fortune
 	yourFortune.classList.add('fortune-show');
 };
+
+//Event handler
+button.onclick = shakeBall;
